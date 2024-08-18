1d56c4bf1d7691464e4afd6ce5251feb2f358ecb
Ever Francisco Ponce Lara <ever864@gmail.com>
2024-08-18 00:00:42 -0500

    feat: change new color

--- src/World/components/scene.js
+++ src/World/components/scene.js
@@ -3,7 +3,7 @@ import { Color, Scene } from "three";
 function createScene() {
   const scene = new Scene();
 
-  scene.background = new Color(0x000000);
+  scene.background = new Color("red");
 
   return scene;
 }
