7648594bed7d1a73ade441a0a6846e2ccf59c0e8
Ever Francisco Ponce Lara <ever864@gmail.com>
2024-08-18 00:27:04 -0500

    feat: complete setup Reziser class

--- src/World/systems/Resizer.js
+++ src/World/systems/Resizer.js
@@ -1,5 +1,17 @@
 class Resizer {
-  constructor() {}
+  constructor(container, camera, renderer) {
+    //Set the camera aspect ratio to match the container's aspect ratio
+    camera.aspect = container.clientWidth / container.clientHeight;
+
+    //Update the camera's projection matrix
+    camera.updateProjectionMatrix();
+
+    //Update the size of the renderer AND the camera
+    renderer.setSize(container.clientWidth, container.clientHeight);
+
+    // Set the pixel ratio (for mobile devices)
+    renderer.setPixelRatio(window.devicePixelRatio);
+  }
 }
 
 export { Resizer };
