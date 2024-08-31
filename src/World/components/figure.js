3c2198b8a82603294132098ad667ef88fa74391a
Ever Francisco Ponce Lara <ever864@gmail.com>
2024-08-31 00:29:42 -0500

    enhancement

--- src/World/components/figure.js
+++ src/World/components/figure.js
@@ -1,17 +1,20 @@
-import { BoxGeometry, Mesh, MeshBasicMaterial, SphereGeometry } from "three";
+import { BoxGeometry, Mesh, MeshStandardMaterial, SphereGeometry } from "three";
 
 function createFigure(figure, width, height, depth, x, y, z) {
  
   if (figure === "cube") {
     const geometry = new BoxGeometry(width, height, depth);
-    const material = new MeshBasicMaterial({ color: "yellow" });
+    const material = new MeshStandardMaterial({ color: "peachpuff" });
     const cube = new Mesh(geometry, material);
+
+    cube.rotation.set(-0.5, -1.1, 0.8);
+
     cube.position.set(x, y, z);
     
     return cube;
   } else if (figure === "sphere") {
     const geometry = new SphereGeometry(width, height, depth);
-    const material = new MeshBasicMaterial({ color: "blue" });
+    const material = new MeshStandardMaterial({ color: "blue" });
     const sphere = new Mesh(geometry, material);
     sphere.position.set(x, y, z);
     return sphere;
--- src/World/components/lights.js
+++ src/World/components/lights.js
@@ -1,7 +1,7 @@
 import { DirectionalLight } from "three";
 
 function createLights() {
-  const light = new DirectionalLight(0xffffff, 1);
+  const light = new DirectionalLight("white", 1);
   light.position.set(10, 10, 10);
 
   return light;
