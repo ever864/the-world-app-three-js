930722cffae3a04c374d47f09d3dc540fb8ec604
Ever Francisco Ponce Lara <ever864@gmail.com>
2024-09-03 23:40:05 -0500

    feat: add new material

--- src/World/components/figure.js
+++ src/World/components/figure.js
@@ -1,16 +1,25 @@
-import { BoxGeometry, Mesh, MeshStandardMaterial, SphereGeometry } from "three";
+import {
+  BoxGeometry,
+  Mesh,
+  MeshStandardMaterial,
+  SphereGeometry,
+  MeshLambertMaterial,
+  TextureLoader,
+} from "three";
 
 function createFigure(figure, width, height, depth, x, y, z) {
- 
   if (figure === "cube") {
     const geometry = new BoxGeometry(width, height, depth);
-    const material = new MeshStandardMaterial({ color: "peachpuff" });
-    const cube = new Mesh(geometry, material);
+    const material = new MeshLambertMaterial({ color: "brown" });
 
+    const cube = new Mesh(geometry, material);
+    cube.alphaMap = new TextureLoader().load(
+      "https://cdn.pixabay.com/photo/2016/02/25/16/33/fruit-1222438_1280.png",
+    );
     cube.rotation.set(-0.5, -1.1, 0.8);
 
     cube.position.set(x, y, z);
-    
+
     return cube;
   } else if (figure === "sphere") {
     const geometry = new SphereGeometry(width, height, depth);
@@ -19,7 +28,6 @@ function createFigure(figure, width, height, depth, x, y, z) {
     sphere.position.set(x, y, z);
     return sphere;
   }
-
 }
 
 export { createFigure };
